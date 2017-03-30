9759fd49dadcecde3ed54efdb720f766539278cc
soultka <soultka@baghansol-ui-MacBook-Pro.local>
2017-03-30 18:15:31 +0900

    pull request

--- closure_practice.playground/Contents.swift
+++ closure_practice.playground/Contents.swift
@@ -8,11 +8,14 @@ let words:[String] = ["a","b", "c", "d", "e", "f"]
 let spell = "a"
 var result:[String] = []
 
-for word in words {
+/*for word in words {
     if (word == spell){
         result += [word]
     }
-}
+}*/
+
+result = words.filter { $0 == "a"}.map{$0}
+
 
 print (result)
 
@@ -22,11 +25,14 @@ print (result)
 let fish = [118, 61, 38, 91, 44, 315, 23, 145, 134, 452]
 var results:[Int] = []
 
-for num in fish {
+/*for num in fish {
     if (num % 2 == 0){
         results += [num]
     }
-}
+}*/
+
+results = fish.filter{ $0 % 2 == 0}.map{$0}
+
 print (results)
 
 // 위에서 구한 짝수들의 합 구하기
@@ -39,7 +45,9 @@ for num in fishs {
         results_ += num
     }
 }
-print (results_)
+results_ = fishs.filter{ $0 % 2 == 0}.reduce(0){$0 + $1}
+
+print(results_)
 
 
 
@@ -53,6 +61,7 @@ for i in 0...4 {
     print("\n")
 }
 
+(0...4).filter
 //카운트다운
 //10부터 줄어들어 0대신 발사 메시지.
 
@@ -64,5 +73,8 @@ for i in 0...10 {
         print("Nuclear launched detected")
     }
 }
-
+let istrue = (0...10).filter{ print(10 - $0)
+                 return $0 == 10
+    }.filter{ _ in print ("Nuclear laungched detected")
+        return true}
 
